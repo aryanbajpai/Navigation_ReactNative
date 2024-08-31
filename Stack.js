b0e28173e60f4144e266b745860ef12c62449e3b
aryanbajpai <bajpaiaryan1@gmail.com>
2024-08-31 18:03:04 +0530

    changes

--- Stack.js
+++ Stack.js
@@ -4,10 +4,12 @@ import StackNav from "./screens/StackNav";
 import DrawerNav from "./screens/DrawerNav";
 import HomePage from "./screens/HomePage";
 import TabNav from "./screens/TabNav";
+import DrawerComp from "./Drawer";
 
 const Stack = createNativeStackNavigator();
 
 const StackComp = () => {
+
   return (
     <Stack.Navigator
       initialRouteName="Home"
@@ -20,7 +22,7 @@ const StackComp = () => {
         headerTitleStyle: { fontWeight: "bold", fontSize: 21 },
         headerRight: () => (
           <Pressable
-            onPress={() => alert("Menu button pressed!")}
+            onPress={() => alert("Menu button clicked!")}
             accessibilityLabel="Menu"
             accessibilityHint="Opens the menu"
           >
@@ -37,36 +39,15 @@ const StackComp = () => {
         name="Home"
         component={HomePage}
         options={{
-          title: "Welcome Home",
-          //forSingle Screen only
-          // headerStyle: {
-          //   backgroundColor: '#3d1f1d'
-          // },
-          // headerTintColor: '#fff',
-          // headerTitleStyle: { fontWeight: 'bold', fontSize: 21},
-          // headerRight: () => (
-          //   <Pressable onPress={() => alert('Menu button pressed!')}>
-          //     <Text style={{color: '#fff', fontSize: 16}}>Menu</Text>
-          //   </Pressable>
-          // ),
-          // contentStyle:{
-          //   backgroundColor: '#df643f'
-          // }
+          title: "Welcome LOGO",
         }}
       />
 
       <Stack.Screen name="Stack" component={StackNav} />
       <Stack.Screen
         name="Drawer"
-        component={DrawerNav}
-        // initialParams={{
-        //   name: "Default Name",
-        // }}
-
-        //Can be used when passing Static Data
-        // options={({ route }) => ({    //To pass Dynamic Title through NAVIGATION
-        //   title: route.params.name
-        // })}
+        component={DrawerComp}
+        options={{ headerShown: false }}
       />
       <Stack.Screen name="Tab" component={TabNav} />
     </Stack.Navigator>
@@ -74,3 +55,69 @@ const StackComp = () => {
 };
 
 export default StackComp;
+
+
+
+
+{/* <Stack.Navigator
+  initialRouteName="Home"
+  screenOptions={{
+    //for all the screens
+    headerStyle: {
+      backgroundColor: "#3d1f1d",
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: { fontWeight: "bold", fontSize: 21 },
+    headerRight: () => (
+      <Pressable
+        onPress={() => alert("Menu button clicked!")}
+        accessibilityLabel="Menu"
+        accessibilityHint="Opens the menu"
+      >
+        <Text style={{ color: "#fff", fontSize: 16 }}>Menu</Text>
+      </Pressable>
+    ),
+
+    contentStyle: {
+      backgroundColor: "#df643f",
+    },
+  }}
+>
+  <Stack.Screen
+    name="Home"
+    component={HomePage}
+    options={{
+      title: "Welcome Home",
+      //forSingle Screen only
+      // headerStyle: {
+      //   backgroundColor: '#3d1f1d'
+      // },
+      // headerTintColor: '#fff',
+      // headerTitleStyle: { fontWeight: 'bold', fontSize: 21},
+      // headerRight: () => (
+      //   <Pressable onPress={() => alert('Menu button pressed!')}>
+      //     <Text style={{color: '#fff', fontSize: 16}}>Menu</Text>
+      //   </Pressable>
+      // ),
+      // contentStyle:{
+      //   backgroundColor: '#df643f'
+      // }
+    }}
+  />
+
+  <Stack.Screen name="Stack" component={StackNav} />
+  <Stack.Screen
+    name="Drawer"
+    component={DrawerComp}
+    options={{ headerShown: false }}
+    // initialParams={{
+    //   name: "Default Name",
+    // }}
+
+    //Can be used when passing Static Data
+    // options={({ route }) => ({    //To pass Dynamic Title through NAVIGATION
+    //   title: route.params.name
+    // })}
+  />
+  <Stack.Screen name="Tab" component={TabNav} />
+</Stack.Navigator>; */}
